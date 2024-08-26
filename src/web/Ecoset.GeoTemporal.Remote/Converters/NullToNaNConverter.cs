b392581ea3d53b759eaa8b5d4ac318d3115be608
Andrew Martin <AndrewIOM@users.noreply.github.com>
2024-08-26 15:35:03 +0100

    Fix handling null and bump system.text.json

--- src/web/Ecoset.GeoTemporal.Remote/Converters/NullToNaNConverter.cs
+++ src/web/Ecoset.GeoTemporal.Remote/Converters/NullToNaNConverter.cs
@@ -9,18 +9,21 @@ namespace Ecoset.GeoTemporal.Remote
     public class NullToNaNConverter : JsonConverter<float>
     {
         public override float Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
-            var str = reader.GetString();
-            if (str == null) {
+            reader.Read();
+            if (reader.TokenType == JsonTokenType.Null)
+            {
                 return System.Single.NaN;
-            } else {
-                return System.Single.Parse(str);
             }
+            if (reader.TokenType != JsonTokenType.Number)
+            {
+                throw new JsonException();
+            }
+            return reader.GetSingle();
         }
 
         public override void Write(Utf8JsonWriter writer, float value, JsonSerializerOptions options)
         {
             JsonSerializer.Serialize(writer, value, value.GetType(), options);
         }
-
     }
 }
